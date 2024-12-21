0257bc59f06a0fb73bb7b4fef448358f195ef5e7
Steven Sutarjie <stevensutarjie14@gmail.com>
2024-12-21 12:55:51 +0700

    updated

--- app/layout/mainLayout/page.tsx
+++ app/layout/mainLayout/page.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import Navbar from '../navbar/_components/navbar';
-import Sidebar from '../sidebar/_components/sidebar';
+import React from "react";
+import Navbar from "../navbar/_components/navbar";
+import Sidebar from "../sidebar/_components/sidebar";
 
 const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-screen">
-      {/* Sidebar tetap */}
-      <Sidebar />
+    <div className="flex h-screen overflow-hidden">
+      {/* Sidebar */}
+      <aside className="w-64 bg-[#182237] flex-shrink-0">
+        <Sidebar />
+      </aside>
 
-      {/* Kontainer utama */}
-      <div className="flex flex-col flex-1 bg-[#0D1B2A]">
-        {/* Navbar tetap */}
-        <div className="mt-5 top-3 z-10 mx-5">
+      {/* Kontainer Utama */}
+      <div className="flex-1 flex flex-col bg-[#0D1B2A] overflow-auto">
+        {/* Navbar */}
+        <header className="bg-[#0D1B2A] px-6 py-4 mb-1 mt-2">
           <Navbar />
-        </div>
+        </header>
 
-        {/* Konten utama dengan scrollbar */}
-        <main className="mt-5 flex-1 p-6">
+        {/* Konten Utama */}
+        <main className="flex-1 px-6 py-4">
           {children}
         </main>
       </div>

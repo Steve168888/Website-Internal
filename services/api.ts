7b35bf2369709cf80bb1fa4f6b219648ed58fdb8
Steven Sutarjie <stevensutarjie14@gmail.com>
2025-03-06 09:54:10 +0700

    updated

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,3 @@
-//const BASE_URL = 'https://thesis-production-f387.up.railway.app/api/v1';
-//const BASE_URL = 'https://ffe5b3a5-59aa-4a79-89fe-0cb5765d4819-00-1v07mi4hw82zc.pike.replit.dev/api/v1';
 const BASE_URL = 'http://localhost:5001/api/v1';
 
 export const fetchAPI = async <T>(
